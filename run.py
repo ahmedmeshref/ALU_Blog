5ed75b2c58ee91b9320fccc33e0ae09071236bde
ahmedmeshref <a.meshref@alustudent.com>
2020-05-13 17:51:30 +0200

    All TODO is marked

--- run.py
+++ run.py
@@ -4,18 +4,3 @@ from blog import app
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-
-"""
-TODO:
-1- Search results should have pagination
-2- Replace the pagination with a show more button
-4- Add post with Fetch (to be done)
-
-
-Advanced:
-1- View all online users
-2- Chat with friends
-3- Use Fetch requests
-4- Update the new post text area 
-"""
