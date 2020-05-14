2a66a9d4e5f5c99c30ee2e2196dceec241c54f53
ahmedmeshref <a.meshref@alustudent.com>
2020-05-14 16:28:36 +0200

    Install a env

--- blog/config.py
+++ blog/config.py
@@ -1,24 +1,24 @@
 import os
 
 
-# class Config:
-#     SECRET_KEY = os.environ.get('SECRETKEY')
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get('EMAIL')
-#     MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRESQL')
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
 class Config:
-    SECRET_KEY = "472267410d1672db337b9e9ca710ee1b"
+    SECRET_KEY = os.environ.get('SECRETKEY')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "a.meshref@alustudent.com"
-    MAIL_PASSWORD = "250787388219@Meshref"
-    SQLALCHEMY_DATABASE_URI = "postgres://rmeucwxwmsazxu:4b4c60b835702b91f9a77f1cf7f324279d 0b0ae58846052fbffdf9db106e" \
-                              "e213@ec2-3-231-16-122.compute-1.amazonaws.com:5432/d3994g9lcuuvtr"
+    MAIL_USERNAME = os.environ.get('EMAIL')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRESQL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# class Config:
+#     SECRET_KEY = "472267410d1672db337b9e9ca710ee1b"
+#     MAIL_SERVER = 'smtp.googlemail.com'
+#     MAIL_PORT = 587
+#     MAIL_USE_TLS = True
+#     MAIL_USERNAME = "a.meshref@alustudent.com"
+#     MAIL_PASSWORD = "250787388219@Meshref"
+#     SQLALCHEMY_DATABASE_URI = "postgres://rmeucwxwmsazxu:4b4c60b835702b91f9a77f1cf7f324279d 0b0ae58846052fbffdf9db106e" \
+#                               "e213@ec2-3-231-16-122.compute-1.amazonaws.com:5432/d3994g9lcuuvtr"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
