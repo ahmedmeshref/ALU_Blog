096466fa2b6f65c94efe78174c3de0f88d24f312
ahmedmeshref <a.meshref@alustudent.com>
2021-06-11 19:09:34 +0200

    Update requirements

--- blog/config.py
+++ blog/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRETKEY')
+    SECRET_KEY = os.urandom(32)
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
